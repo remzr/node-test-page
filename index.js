45e4bc24e4180fdc0b983620f40e1afbc51206e3
remzr <110097561+remzr@users.noreply.github.com>
2026-01-21 22:00:15 +0100

    Add routing

--- index.js
+++ index.js
@@ -9,8 +9,21 @@ const port = 3000;
 // Create a local server
 const server = http.createServer((req, res) => {
     
+    let filePath; 
+
+    //Which file has to be read?
+    if (req.url === '/' || req.url === '/index.html') {
+        filePath = __dirname + '/index.html';
+    } else if (req.url === '/about.html') {
+        filePath = __dirname + '/about.html';
+    } else if (req.url === '/contact-me.html') {
+        filePath = __dirname + '/contact-me.html';
+    } else {
+        filePath = __dirname + '/404.html';
+    }
+
     //Runs on every request
-    fs.readFile(__dirname + '/index.html', (err, html) => {
+    fs.readFile(filePath, (err, html) => {
         
         //Runs when file is rdy
         if(err){
@@ -19,7 +32,7 @@ const server = http.createServer((req, res) => {
             return;
         }
 
-        res.statusCode = 200;
+        res.statusCode = (filePath.includes('404.html')) ? 404 : 200;
         res.setHeader('Content-Type', 'text/html');
         res.end(html);
     });
