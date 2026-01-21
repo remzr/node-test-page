461c09dc6ff9488d89f1ecfdf7c28fa5da4267f0
remzr <110097561+remzr@users.noreply.github.com>
2026-01-21 21:45:43 +0100

    Move readfile in requesthandler

--- index.js
+++ index.js
@@ -6,21 +6,26 @@ const fs = require('fs');
 const hostname = '127.0.0.1';
 const port = 3000;
 
-//Get index.html
-fs.readFile('index.html', (err, html) => {
-    if(err){
-        throw err;
-    }
-});
-
 // Create a local server
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.write(html);
-    res.end();
+    
+    //Runs on every request
+    fs.readFile('index.html', (err, html) => {
+        
+        //Runs when file is rdy
+        if(err){
+            res.statusCode = 500;
+            res.end('Server error');
+            return;
+        }
+
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        res.end(html);
+    });
 });
 
+//Start listening
 server.listen(port, hostname, ()=> {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
