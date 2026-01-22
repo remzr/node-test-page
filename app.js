6328abc453298260fe854c86567e413ae98bff76
remzr <110097561+remzr@users.noreply.github.com>
2026-01-22 22:25:41 +0100

    Separate readHTML from router function

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
 #!/usr/bin/node
 
-const http = require('node:http');
+const express = require("express");
+const app = express();
+
 const fs = require('fs');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const PORT = 3000;
+
+function readHTML(path, res) {
+    fs.readFile(path, (err, html) => {
+        
+        //Runs when file is rdy
+        if(err){
+            res.statusCode = 500;
+            res.end('Server error');
+            return;
+        }
+
+        res.statusCode = (path.includes('404.html')) ? 404 : 200;
+        res.setHeader('Content-Type', 'text/html');
+        res.end(html);
+    });
+}    
 
 // Create a local server
-const server = http.createServer((req, res) => {
+app.get("/", (req, res) => {
     
     let filePath; 
 
@@ -23,22 +40,13 @@ const server = http.createServer((req, res) => {
     }
 
     //Runs on every request
-    fs.readFile(filePath, (err, html) => {
-        
-        //Runs when file is rdy
-        if(err){
-            res.statusCode = 500;
-            res.end('Server error');
-            return;
-        }
-
-        res.statusCode = (filePath.includes('404.html')) ? 404 : 200;
-        res.setHeader('Content-Type', 'text/html');
-        res.end(html);
-    });
+    readHTML(filePath, res);
 });
 
 //Start listening
-server.listen(port, hostname, ()=> {
-    console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(PORT, (error) => {
+  if (error) {
+    throw error;
+  }
+  console.log(`Server running on ${PORT}!`);
 });
